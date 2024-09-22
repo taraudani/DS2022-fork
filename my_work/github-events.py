4d9e1097e817196db5282d1233372aa525eadbb7
taraudani <157422972+taraudani@users.noreply.github.com>
2024-09-22 14:04:21 +0000

    Python file edited and finished

--- my_work/github-events.py
+++ my_work/github-events.py
@@ -7,4 +7,13 @@ import requests
 
 GHUSER = os.getenv('GITHUB_USER')
 
-print(GHUSER)
+
+url = 'https://api.github.com/users/{0}/events'.format(GHUSER)
+
+r = json.loads(requests.get(url).text)
+
+for x in r[:5]:
+  event = x['type'] + ' :: ' + x['repo']['name']
+  print(event)
+
+print(r)
