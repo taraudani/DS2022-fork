2abd8d85996271887dbe8345e97cc1491bc3e685
taraudani <157422972+taraudani@users.noreply.github.com>
2024-09-22 00:46:42 +0000

    Finalized working madlib

--- my_work/madlib.sh
+++ my_work/madlib.sh
@@ -13,4 +13,4 @@ read -p "6. Name something you hate doing: " VERB2
 read -p "7. Name an adverb: " ADV1
 read -p "8. Give me an adverb: " ADV2
 
-echo "I love playing" NOUN1 "! Especially when my best friend" NOUN2 "plays! She is super" ADJ1 "at it. We also" VERB1 "but I am" ADV1 ADJ2 "at that." NOUN2 "is" ADV2 "at" VERB2.
+echo "I love playing" $NOUN1 "! Especially when my best friend" $NOUN2 "plays! They are super" $ADJ1 "at it. We also" $VERB1 "but I am" $ADV1 $ADJ2 "at that." $NOUN2 "is" $ADV2 "at" $VERB2.
